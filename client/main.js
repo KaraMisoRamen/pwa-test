7ce50e1178c768a41981b0f01cef7615df113d50
樊 <pei.fuan@jp.oro.com>
2020-01-28 21:00:39 +0900

    test

---
+++ client/main.js
@@ -0,0 +1,49 @@
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+const publicVapidKey = 'BOK-YxcIPGxxywWYDgFMLVF6rw20w2tiy3j4Dg2VDXYkc07SBJn_SguGcyJUj4XwYuK7_-XoH9DE4xJf5d3x3zQ';
+
+const triggerPush = document.querySelector('.trigger-push');
+
+alert(navigator)
+
+async function triggerPushNotification() {
+  if ('serviceWorker' in navigator) {
+    const register = await navigator.serviceWorker.register('/sw.js', {
+      scope: '/'
+    });
+
+    alert('waiting for acceptance');
+    const subscription = await register.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+    });
+    console.log('acceptance complete');
+
+    await fetch('/subscribe', {
+      method: 'POST',
+      body: JSON.stringify(subscription),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } else {
+    console.error('Service workers are not supported in this browser');
+  }
+}
+
+triggerPush.addEventListener('click', () => {
+  triggerPushNotification().catch(error => console.error(error));
+});
